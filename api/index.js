8091cd7e9797d009230a3c8700c7538d77eaee25
Elias-Dewa <elias.dewa19@gmail.com>
2024-02-15 04:02:20 -0800

    connect database to the server

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB).then(() => console.log('MongoDb is connected')).catch(error => console.log(error));
 
 const app = express();
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <Navbar className='border-b-2'>
       <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-indigo-500 rounded-lg text-white'>My</span>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Elias's</span>
         Blog
       </Link>
       <form>
@@ -23,7 +23,7 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 sm:inline' color='gray' pill>
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
           <FaMoon />
         </Button>
         <Link to="/sign-in">
