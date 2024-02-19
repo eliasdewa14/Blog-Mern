bc08a194e1fc9ddbf51d15a0026adee45ad98f69
Elias-Dewa <elias.dewa19@gmail.com>
2024-02-19 11:31:57 -0800

    create the footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,80 @@
+import { Footer } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble, BsLinkedin } from 'react-icons/bs';
+export default function FooterCom() {
+  return (
+    <Footer container className='border-t-2'>
+      <div className='w-full max-w-7xl mx-auto'>
+        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+          <div className='mt-5'>
+            <Link
+              to='/'
+              className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
+            >
+              <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+                Elias's
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+            <div>
+              <Footer.Title title='About' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='https://github.com/eliasdewa14/javascript-projects'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  JS Projects
+                </Footer.Link>
+                <Footer.Link
+                  href='/about'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Elias's Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Follow us' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='https://www.github.com/eliasdewa14'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href='https://www.linkedin.com/in/eliasdewa14/'>LinkedIn</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Legal' />
+              <Footer.LinkGroup col>
+                <Footer.Link href='#'>Privacy Policy</Footer.Link>
+                <Footer.Link href='#'>Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className='w-full sm:flex sm:items-center sm:justify-between'>
+          <Footer.Copyright
+            href='#'
+            by="Elias's blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href='https://facebook.com/eliasdewa14' icon={BsFacebook} target='_blank'/>
+            <Footer.Icon href='https://twitter.com/eliasdewa14' icon={BsTwitter} target='_blank'/>
+            <Footer.Icon href='https://github.com/eliasdewa14' icon={BsGithub} target='_blank'/>
+            <Footer.Icon href='https://www.linkedin.com/in/eliasdewa14/' icon={BsLinkedin} target='_blank'/>
+
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
