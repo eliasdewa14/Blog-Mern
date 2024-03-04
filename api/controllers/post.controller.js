c79c10ed495d20303ba263c42d73688c4a88bc55
Elias-Dewa <elias.dewa19@gmail.com>
2024-03-04 12:16:03 -0800

    create get post api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -24,3 +24,38 @@ export const createPost = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getPosts = async (req, res, next) => {
+  try {
+    // Get some posts and remaining posts after show more button clicked
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    // Sort directions
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+    // Create posts
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId}),
+      ...(req.query.category && { category: req.query.category}),
+      ...(req.query.slug && { slug: req.query.slug}),
+      ...(req.query.postId && { _id: req.query.postId}),
+      ...(req.query.searchTerm && {
+        $or: [
+          {title: {$regex: req.query.searchTerm, $option: "i"}},
+          {content: {$regex: req.query.searchTerm, $option: "i"}},
+        ]
+      }),
+    }).sort({ updatedAt: sortDirection}).skip(startIndex).limit(limit)
+    // To get the total posts
+    const totalPosts = await Post.countDocuments();
+    // To get the total posts in last month
+    const now = new Date();
+    const oneMonthAgo = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
+    
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: {$gte: oneMonthAgo}
+    });
+    res.status(200).json({posts, totalPosts, lastMonthPosts});
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -49,8 +49,8 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={() => dispatch(toggleTheme())}>
-          {theme === 'light' ? <FaSun /> : <FaMoon />}
+        <Button className='hidden sm:inline' color='gray' pill onClick={() => dispatch(toggleTheme())}>
+          {theme === 'light' ? <FaSun className='text-xl' /> : <FaMoon className='text-xl' />}
         </Button>
         {currentUser ? (
           <Dropdown arrowIcon={false} inline label={
