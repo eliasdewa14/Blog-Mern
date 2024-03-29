dd9a1a1898935ab50f7c7f3f3286b3027e3cded1
Elias-Dewa <elias.dewa19@gmail.com>
2024-03-29 04:23:41 -0700

    add comment section to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import { Sidebar } from 'flowbite-react';
 import { useEffect, useState } from 'react';
-import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup } from 'react-icons/hi'
+import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiAnnotation } from 'react-icons/hi'
 import { Link, useLocation } from 'react-router-dom';
 
 import { useDispatch } from 'react-redux';
@@ -54,9 +54,26 @@ export default function DashSidebar() {
             </Link>
           }
           {currentUser.isAdmin &&
-            <Link to='/dashboard?tab=users'>
-              <Sidebar.Item active={tab === 'users'} icon={HiOutlineUserGroup} as="div">Users</Sidebar.Item>
-            </Link>
+            <>
+              <Link to='/dashboard?tab=users'>
+                <Sidebar.Item
+                  active={tab === 'users'}
+                  icon={HiOutlineUserGroup}
+                  as='div'
+                >
+                  Users
+                </Sidebar.Item>
+              </Link>
+              <Link to='/dashboard?tab=comments'>
+                <Sidebar.Item
+                  active={tab === 'comments'}
+                  icon={HiAnnotation}
+                  as='div'
+                >
+                  Comments
+                </Sidebar.Item>
+              </Link>
+            </>
           }
           <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignOut}>
             Sign Out
