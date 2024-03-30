f7c8a0331653ba11cfffb8e56edd3c19ce3d3b20
Elias-Dewa <elias.dewa19@gmail.com>
2024-03-30 03:55:34 -0700

    Complete home page

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -2,7 +2,7 @@ import { Button } from "flowbite-react";
 
 export default function CallToAction() {
   return (
-    <div className="flex flex-col sm:flex-row border border-slate-500 justify-center items-center rounded-tl-3xl rounded-br-3xl p-3">
+    <div className="flex flex-col sm:flex-row border border-slate-500 justify-center items-center rounded-tl-3xl rounded-br-3xl p-3 bg-amber-100 dark:bg-slate-700">
       <div className="flex-1 justify-center flex flex-col items-center">
         <h2 className="text-3xl">Want to learn JS</h2>
         <p className="text-gray-500 my-2">Check out these resources to learn more about JavaScript</p>
